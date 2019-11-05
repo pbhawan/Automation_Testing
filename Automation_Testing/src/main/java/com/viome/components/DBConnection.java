cbcea5eef24736aa4875481c3414b765134ce438
Pratibha <pratibhabhawan20@gmail.com>
2019-11-05 16:02:31 +0530

    Updated DBConnection file

--- Automation_Testing/src/main/java/com/viome/components/DBConnection.java
+++ Automation_Testing/src/main/java/com/viome/components/DBConnection.java
@@ -8,46 +8,37 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-	// Redshift driver:
-	// "jdbc:redshift://x.y.us-west-2.redshift.amazonaws.com:5439/dev";
 	static final String dbURL = "jdbc:redshift://viome-metacube-cluster.cmcvkrhcw0ac.us-east-2.redshift.amazonaws.com:5439/viomedev";
 	static final String MasterUsername = "viome";
 	static final String MasterUserPassword = "METAViomecube3";
 	ResultSet rs;
 	ConnectionProperties _CP = new ConnectionProperties();
+
 	public ConnectionProperties GetRecordFromDB(JSONObject CustomerJsonData, String webhook) throws SQLException {
-	
+
 		String id = CustomerJsonData.get("id").toString();
 		try {
 			System.out.println("Listing system tables...");
 			_CP.stmt = _CP.conn.createStatement();
 			String sql;
-			if(webhook=="Customer")
-			{
-			sql = "SELECT * FROM public.customer where id=" + id + ";";
-			_CP.rs = _CP.stmt.executeQuery(sql);
+			if (webhook == "Customer") {
+				sql = "SELECT * FROM public.customer where id=" + id + ";";
+				_CP.rs = _CP.stmt.executeQuery(sql);
 			}
-			if(webhook=="Location")
-			{
-			sql = "SELECT * FROM public.location where id=" + id + ";";
-			_CP.rs = _CP.stmt.executeQuery(sql);
+			if (webhook == "Location") {
+				sql = "SELECT * FROM public.location where id=" + id + ";";
+				_CP.rs = _CP.stmt.executeQuery(sql);
 			}
-			if(webhook=="Theme")
-			{
-			sql = "SELECT * FROM public.theme where id=" + id + ";";
-			_CP.rs = _CP.stmt.executeQuery(sql);
+			if (webhook == "Theme") {
+				sql = "SELECT * FROM public.theme where id=" + id + ";";
+				_CP.rs = _CP.stmt.executeQuery(sql);
 			}
-			if(webhook=="Product")
-			{
-				
-			sql = "SELECT * FROM public.product where id=" + id + ";";			
-			_CP.rs = _CP.stmt.executeQuery(sql);
+			if (webhook == "Product") {
+
+				sql = "SELECT * FROM public.product where id=" + id + ";";
+				_CP.rs = _CP.stmt.executeQuery(sql);
 			}
-//			if(webhook=="Prodvarient")
-//			{
-//			sql = "SELECT * FROM public.variants where id=" + id + ";";
-//			_CP.rs = _CP.stmt.executeQuery(sql);
-//			}
+//		
 		} catch (Exception ex) {
 			// For convenience, handle all errors here.
 			ex.printStackTrace();
@@ -57,36 +48,24 @@ public class DBConnection {
 
 		return _CP;
 	}
-	
+
 	public ConnectionProperties DBConnection() throws SQLException {
 		try {
-			// Dynamically load driver at runtime.
-			// Redshift JDBC 4.1 driver: com.amazon.redshift.jdbc41.Driver
-			// Redshift JDBC 4 driver: com.amazon.redshift.jdbc4.Driver
-			Class.forName("com.amazon.redshift.jdbc.Driver");
 
-			// Open a connection and define properties.
+			Class.forName("com.amazon.redshift.jdbc.Driver");
 			System.out.println("Connecting to database...");
 			Properties props = new Properties();
-
-			// Uncomment the following line if using a keystore.
-			// props.setProperty("ssl", "true");
 			props.setProperty("user", MasterUsername);
 			props.setProperty("password", MasterUserPassword);
 			_CP.conn = DriverManager.getConnection(dbURL, props);
-
-			// Try a simple query.
-			System.out.println("Listing system tables...");
 			_CP.stmt = _CP.conn.createStatement();
-	}catch (Exception ex) {
-		// For convenience, handle all errors here.
-		ex.printStackTrace();
-	} finally {
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
 
-	}
+		}
 		return _CP;
 
 	}
-	
-	
+
 }
