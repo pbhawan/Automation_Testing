44ba52a90d71e47118b02ceec7dba6e8de2ef770
Nikhil <Nikhil@Nikhil-Jain1>
2019-11-05 11:45:00 +0530

    Trying new Commit

--- Automation_Testing/src/test/java/testCases/Customer.java
+++ Automation_Testing/src/test/java/testCases/Customer.java
@@ -27,7 +27,7 @@ import com.viome.components.ExcelToJSONConvertor;
 import com.viome.components.HTTPConnection;
 import com.viome.enums.webhooks;
 
-/* git changes a*/
+/* git changes b*/
 
 public class Customer {
 	HTTPConnection HC;
