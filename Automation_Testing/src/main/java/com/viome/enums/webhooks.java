53dfc22db73ca74511492ca235a29c92b76cc19c
Pratibha <pratibhabhawan20@gmail.com>
2019-11-08 15:52:24 +0530

    Added more webhooks

--- Automation_Testing/src/main/java/com/viome/enums/webhooks.java
+++ Automation_Testing/src/main/java/com/viome/enums/webhooks.java
@@ -1,7 +1,19 @@
 package com.viome.enums;
 
 public enum webhooks {
-	Customer, Location, Theme,Product,Cart,Order,DraftOrder,Checkout;
+	Customer, 
+	Location, 
+	Theme,
+	Product,
+	Cart,
+	Order,
+	DraftOrder,
+	Checkout,
+	Refund, 
+	Fulfillment,
+	Order_Transaction,
+	Tender_Transaction,
+	Shop;
 
 }
 
